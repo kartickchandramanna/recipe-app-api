1787c8a42202916e5b21f6666029d3df411d52b3
Kartick Chandra Manna <kartick@solivatech.com>
2024-07-05 19:53:57 +0530

    complete section 8 lec-43

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+""" 
+calculator function 
+"""
+
+def add(x,y):
+    """ add x and y and return the result """
+    return x + y
+
+def sub(x,y):
+    """subtract x from y and return the result."""
+    return y - x
