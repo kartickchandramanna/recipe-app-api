fbc9c0c0998ceb6f7cce276982277a3a8bc91c61
Kartick Chandra Manna <kartick@solivatech.com>
2024-07-25 12:28:17 +0530

    completed sec-94

--- app/app/settings.py
+++ app/app/settings.py
@@ -45,7 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
-    'drf_spectacular'
+    'rest_framework.authtoken',
+    'drf_spectacular',
+    'user',
+    'recipe',
 ]
 
 MIDDLEWARE = [
